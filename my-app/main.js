a635a6728b73d3d0e5e328055f321794c70030e6
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-09-25 16:29:46 +0100

    progress

--- my-app/main.js
+++ my-app/main.js
@@ -1,15 +1,40 @@
 const input = document.querySelector(".main .main-content .input-box input");
 const urlbox = document.querySelector(".shorten-url-box");
 const submit = document.querySelector(".main .main-content .input-box button");
+
 window.copyToClipboard = copyToClipboard;
 
 submit.addEventListener("click", async (event) => {
   event.preventDefault();
   let URL = input.value.trim();
+  const matches = URL.match(/https?:\/\//g);
 
- 
   if (!URL.startsWith("https://") && !URL.startsWith("http://")) {
     URL = "https://" + URL;
+  }
+
+  if (
+    URL === "" ||
+    !URL.includes(".") ||
+    URL.length < 5 ||
+    URL === "." ||
+    URL === "https://." ||
+    URL === "http://." ||
+    URL === ".com" ||
+    URL === "https://.com" ||
+    URL === "http://.com" ||
+    URL === "www." ||
+    URL === "https://www." ||
+    URL === "http://www." 
+
+  ) {
+    alert("Please enter a valid URL");
+    return;
+  }
+
+  if (matches && matches.length > 1) {
+    alert("Please enter a valid URL");
+    return;
   }
 
   try {
@@ -28,7 +53,6 @@ submit.addEventListener("click", async (event) => {
     const data = await response.json();
     const fullShortUrl = `${window.location.origin}/${data.shortUrl}`;
 
-    
     urlbox.innerHTML = `
       <input id="ShortenUrl" type="text" readonly value="${fullShortUrl}" />
       <button onclick="copyToClipboard('ShortenUrl')">COPY</button>
@@ -39,7 +63,6 @@ submit.addEventListener("click", async (event) => {
     console.error("Error:", error);
   }
 });
-
 
 const path = location.pathname.slice(1);
 if (path) {
@@ -53,17 +76,16 @@ if (path) {
     .catch((err) => console.error("Fetch failed:", err));
 }
 
-
 function copyToClipboard(inputId) {
   const input = document.getElementById(inputId);
   input.select();
-  input.setSelectionRange(0, 99999); // for mobile devices
 
-  navigator.clipboard.writeText(input.value)
+  navigator.clipboard
+    .writeText(input.value)
     .then(() => {
       alert("Copied to clipboard!");
     })
-    .catch(err => {
+    .catch((err) => {
       console.error("Failed to copy: ", err);
     });
 }
